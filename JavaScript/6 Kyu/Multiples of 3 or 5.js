cd91195838d33479bff4e25076ef0e7ebf6ff679
Lucide-Pascale <lucpas15nov@gmail.com>
2025-07-10 14:46:40 +0200

    Multiples of 3 or 5/ 6Kyu Kata with comments

--- JavaScript/6 Kyu/Multiples of 3 or 5.js
+++ JavaScript/6 Kyu/Multiples of 3 or 5.js
@@ -3,17 +3,17 @@ function solution(number){
   let arr2=[];
   for (i=3;i<number;i++){
     if((i%3)===0){
-      arr1.push(i);
+      arr1.push(i); // Array of multiples of 3
     }
   }
   for (i=5;i<number;i++){
     if((i%5)===0){
-      arr1.push(i);
+      arr1.push(i); // Array of with multiples of 5 added
     }
   }
   
-  arr2=[...new Set([...arr1])];
-  return arr2.reduce((acc,curr)=> acc+curr, 0)
+  arr2=[...new Set([...arr1])]; // removing Duplicates
+  return arr2.reduce((acc,curr)=> acc+curr, 0) // Sum of elements in the Array
 }
 
 console.log(solution(10)) //23
