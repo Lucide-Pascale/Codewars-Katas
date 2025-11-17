47d41b33976155ef773e2009f9747c185ae94d5d
Lucide-Pascale <lucpas15nov@gmail.com>
2025-11-17 19:26:07 +0200

    Find In Array 6 Kyu Kata

---
+++ JavaScript/6 Kyu/FindWithinArray.js
@@ -0,0 +1,13 @@
+function findInArray(sequence, func) {
+  for (let i = 0; i < sequence.length; i++) {
+    if (func(sequence[i], i)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+var trueIfEven = function(value, index) { 
+  return (value % 2 === 0); 
+};
+
